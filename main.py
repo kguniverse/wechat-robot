fa79247c53cc7521c1aabefe06eb09a398afbd12
kguniverse <kkwang@outlook.com>
2021-12-13 13:03:55 +0800

    add parser frame

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from fastapi.responses import PlainTextResponse
 import hashlib
 import uvicorn
 import receive, reply
+from parser import parse
 
 app = FastAPI()
 
@@ -19,7 +20,10 @@ async def chatHandler(rq: Request):
         if isinstance(recMsg, receive.Msg) and recMsg.MsgType == 'text':
             toUser = recMsg.FromUserName
             fromUser = recMsg.ToUserName
-            content = "test"
+            # if bytes.decode(recMsg.Content) == '王乾凱是傻子':
+            content = "张裕是傻子"
+            # else:
+            content = bytes.decode(recMsg.Content)
             replyMsg = reply.TextMsg(toUser, fromUser, content)
             return PlainTextResponse(replyMsg.send())
         else:
@@ -31,7 +35,12 @@ async def chatHandler(rq: Request):
 
 # handler config
 @app.post("/config")
-async def configHandler():
+async def configHandler(rq: Request):
+    webData = await rq.body()
+    webStr = bytes.decode(webData)
+    print("config shell is", webStr)
+    parse(str(webStr))
+    return PlainTextResponse("parse success")
     pass
 
 if __name__ == '__main__':
